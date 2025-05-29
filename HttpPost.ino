f2e10c644ee3c509a81684c211eeb778721f0ae7
홍지민 <82097844+lunatic280@users.noreply.github.com>
2025-05-29 15:59:32 +0900

    Create HttpPost.ino

---
+++ HttpPost.ino
@@ -0,0 +1,43 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+
+const char* ssid = "SK_7A74_2.4G";
+const char* password = "ACC32@9068";
+const char* serverUrl = "https://port-0-cpdserver-lzedf6rf40aa38e9.sel4.cloudtype.app/endpoint";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" WiFi connected!");
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClientSecure client;
+    client.setInsecure(); // SSL 인증서 검증 비활성화(테스트용, 실제 서비스시 인증서 등록 권장)
+    HTTPClient https;
+
+    if (https.begin(client, serverUrl)) {
+      https.addHeader("Content-Type", "application/json");
+      String postData = "{\"beatsPerMinute\": 100, \"interBeatInterval\": 80, \"signal\": 23, \"timeStamp\": \"2024-05-23 21:00:00\"}";
+
+      int httpResponseCode = https.POST(postData);
+
+      if (httpResponseCode > 0) {
+        String response = https.getString();
+        Serial.print("Server Response: ");
+        Serial.println(response);
+      } else {
+        Serial.print("Error on sending POST: ");
+        Serial.println(httpResponseCode);
+      }
+      https.end();
+    }
+  }
+  delay(1000);
+}
